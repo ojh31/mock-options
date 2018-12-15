a5cb3be8d91ed75320933a7244872b8e3fc65fef
ojh31 <oskar.hollinsworth@gmail.com>
2018-12-15 17:23:48 +0000

    Sizes working in voice request user market

--- game.py
+++ game.py
@@ -1,24 +1,38 @@
+import random
 from boards import PublicBoard
-from structures import Option
 from user_input import get_user_market
 from sounds import rand_order
+from order_types import IcebergOrder
 
 
 class Mock(object):
     # Game of mock against a bot
 
     def __init__(self):
+        num_clients = 100
         self.publicBoard = PublicBoard()
         self.fairBoard = self.publicBoard.fair
+        self.clients = [IcebergOrder.rand(self.fairBoard)
+                        for _ in range(num_clients)]
         self.play()
 
     def play(self):
-        asset = Option.rand(self.fairBoard)
+        if not self.clients:
+            return 'Game over'
+        order = self.pop()
         print(self.publicBoard)
-        mkt = get_user_market(asset)
+        mkt = get_user_market(order)
         print('Player market: {}'.format(mkt))
         rand_order()
         self.play()
+
+    def pop(self):
+        clients = self.clients
+        client = random.choice(clients)
+        popped = client.pop()
+        if client.is_empty():
+            clients.remove(client)
+        return popped
 
 
 if __name__ == "__main__":
